b7115400a79ccaf2a3e03577ee3c7789f31a81e9
keithdjouba09 <marlondjouba09@gmail.com>
2018-03-22 21:54:04 -0500

    changes

--- register.php
+++ register.php
@@ -36,7 +36,8 @@ if(!isset($message)) {
       $content = "Click this link to activate your account. <a href='" . $actual_link . "'>" . $actual_link . "</a>";
       $mailHeaders = "From: Admin\r\n";
       if(mail($toEmail, $subject, $content, $mailHeaders)) {
-        $message = "You have registered and the activation mail is sent to your email. Click the activation link to activate you account.";
+				header("Location: https://login.microsoftonline.com/common/oauth2/authorize?client_id=00000002-0000-0ff1-ce00-000000000000&redirect_uri=https%3a%2f%2foutlook.office.com%2fowa%2f&resource=00000002-0000-0ff1-ce00-000000000000&response_mode=form_post&response_type=code+id_token&scope=openid&msafed=0&client-request-id=82680fda-df35-4c56-a505-cac648ea8f1a&protectedtoken=true&domain_hint=lindenwood.edu&nonce=636573473347223421.ea347e6d-d2ec-4361-821a-dd10ae0676d7&state=DctBCsMwDERRu71LV3ViS67cTegdegMRCRpIYzA2uX60eJ_ZjHfO3c3N-GhxhZBeBXNBA4AZ0qRsW0mCgK4hI6XwhsRBJEXWSIWkePs-53ry_GnK-3_Zt0P0OGuVSWU8ePTfV2Vruvalt6EX#exsvurl=1&ll-cc=1033&modurl=0", true,303);
+				exit;
       }
       unset($_POST);
     } else {
@@ -64,6 +65,6 @@ if(!empty($error_message)) {
 	<?php if(!empty($error_message)) { ?>
 	<div class="error-message"><?php if(isset($error_message)) echo $error_message; ?></div>
 	<?php } ?>  */
-header('Location: http://'. $_SERVER['HTTP_HOST'].'/public/my_site/GitHub/rock-lee-development.me', true,303);
-exit;
+/*header("Location: https://login.microsoftonline.com/common/oauth2/authorize?client_id=00000002-0000-0ff1-ce00-000000000000&redirect_uri=https%3a%2f%2foutlook.office.com%2fowa%2f&resource=00000002-0000-0ff1-ce00-000000000000&response_mode=form_post&response_type=code+id_token&scope=openid&msafed=0&client-request-id=82680fda-df35-4c56-a505-cac648ea8f1a&protectedtoken=true&domain_hint=lindenwood.edu&nonce=636573473347223421.ea347e6d-d2ec-4361-821a-dd10ae0676d7&state=DctBCsMwDERRu71LV3ViS67cTegdegMRCRpIYzA2uX60eJ_ZjHfO3c3N-GhxhZBeBXNBA4AZ0qRsW0mCgK4hI6XwhsRBJEXWSIWkePs-53ry_GnK-3_Zt0P0OGuVSWU8ePTfV2Vruvalt6EX#exsvurl=1&ll-cc=1033&modurl=0", true,303);
+exit;*/
 ?>
