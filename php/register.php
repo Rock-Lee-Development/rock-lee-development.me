a8a48443cd9a7a0c04a101dfff2a969eb026d8c2
Segun Akinyemi <akintundejr4@gmail.com>
2018-03-28 17:55:28 -0500

    Yes bro

--- php/register.php
+++ php/register.php
@@ -60,14 +60,4 @@ if(!empty($error_message)) {
     if(isset($error_message)) echo $error_message;
 }
 
-
-
-/* 	<?php  ?>
-	<div class="success-message"><?php ?></div>
-	<?php } ?>
-	<?php if(!empty($error_message)) { ?>
-	<div class="error-message"><?php if(isset($error_message)) echo $error_message; ?></div>
-	<?php } ?>  */
-/*header("Location: https://login.microsoftonline.com/common/oauth2/authorize?client_id=00000002-0000-0ff1-ce00-000000000000&redirect_uri=https%3a%2f%2foutlook.office.com%2fowa%2f&resource=00000002-0000-0ff1-ce00-000000000000&response_mode=form_post&response_type=code+id_token&scope=openid&msafed=0&client-request-id=82680fda-df35-4c56-a505-cac648ea8f1a&protectedtoken=true&domain_hint=lindenwood.edu&nonce=636573473347223421.ea347e6d-d2ec-4361-821a-dd10ae0676d7&state=DctBCsMwDERRu71LV3ViS67cTegdegMRCRpIYzA2uX60eJ_ZjHfO3c3N-GhxhZBeBXNBA4AZ0qRsW0mCgK4hI6XwhsRBJEXWSIWkePs-53ry_GnK-3_Zt0P0OGuVSWU8ePTfV2Vruvalt6EX#exsvurl=1&ll-cc=1033&modurl=0", true,303);
-exit;*/
 ?>
