22a3e070412d742b25ec2a71e56cc13c9ce48bfc
keithdjouba09 <marlondjouba09@gmail.com>
2018-04-08 11:47:59 -0500

    forgot password

---
+++ php/forgot_password.php
@@ -0,0 +1,69 @@
+<?php
+	if(!empty($_POST["register-user"])) {
+		/* Form Required Field Validation */
+		foreach($_POST as $key=>$value) {
+			if(empty($_POST[$key])) {
+			$error_message = "All Fields are required";
+			break;
+			}
+		}
+    /* Password Matching Validation */
+if($_POST['newPassword'] != $_POST['confirm_password']){
+$error_message = 'Passwords should be same<br>';
+}
+
+/* Email Validation */
+if(!isset($error_message)) {
+	if (!filter_var($_POST["newEmail"], FILTER_VALIDATE_EMAIL)) {
+	$error_message = "Invalid Email Address";
+	}
+}
+
+if(!isset($message)) {
+  require_once("DBController.php");
+  $db_handle = new DBController();
+  $query = "SELECT * FROM User WHERE email='" . $_POST["emailForm"] . "'";
+  $count = $db_handle->numRows($query);
+  $token = $db_handle->generateNewString();
+
+  if($count>0) {
+    $updateToken= "UPDATE UserToken set Token = '$token' WHERE email='" .$_POST["emailForm"] . "'";
+    $result2 = $db_handle->updateQuery($updateToken);
+    $current_email = $_POST["emailForm"];
+    if(!empty($current_id)) {
+      $actual_link = "http://localhost/public/my_site/GitHub/rock-lee-development.me/php/activate.php?UserID=$current_id &Token=$token";
+      $toEmail = $_POST["newEmail"];
+      $subject = "User Registration Activation Email";
+      $content = "Click this link to activate your account. <a href='" . $actual_link . "'> </a>";
+      $mailHeaders = "From: noreply@tourneyregistration.com\r\n";
+      if(mail($toEmail, $subject, $content, $mailHeaders)) {
+        echo "<script> alert('Your account has been registere. An activation link has been sent to your email.');
+        window.location.href='../index.html'; </script>";
+				exit;
+      }
+      unset($_POST);
+    } else {
+      $message = "Problem in registration. Try Again!";
+    }
+
+
+
+  }
+   else
+   {
+    echo "<script> alert('The email address you entered is already associated with a user account');
+    window.location.href='../index.html'; </script>";
+		exit;
+  }
+}
+}
+
+if(!empty($message)) {
+    if(isset($message)) echo $message;
+}
+
+if(!empty($error_message)) {
+    if(isset($error_message)) echo $error_message;
+}
+
+?>
