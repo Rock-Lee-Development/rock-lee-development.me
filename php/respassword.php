9eb1d2bf3bb1283756ba15f954e927a93434020a
keithdjouba09 <marlondjouba09@gmail.com>
2018-04-08 18:04:11 -0500

    still need to work on it

--- php/respassword.php
+++ php/respassword.php
@@ -2,6 +2,7 @@
 if($_POST['resPassword'] != $_POST['confirm_password']){
 $error_message = 'Passwords should be same<br>';
 }
+
 if(!isset($message)) {
 	require_once("DBController.php");
 	$db_handle = new DBController();
@@ -33,30 +34,33 @@ if(!isset($message)) {
   <link href="css/style.css" type="text/css" rel="stylesheet" />
   </head>
   <body>
-    <div class="modal fade" id="forgotmodal" tabindex="-1" role="dialog" aria-labelledby="myModallabel" aria-hidden="true">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="myModalLabel">Enter the Email Address Associated With Your Account</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body mx-3">
-            <div class="form-group">
-              <input type="password" name="resPassword" id="newPassword" required="required" tabindex="2" class="form-control" placeholder="New Password">
-            </div>
-            <div class="form-group">
-              <input type="password" name="confirm_password" id="confirm-password" tabindex="2" required="required" class="form-control"
-              oninput="checkPasswords(this)" placeholder="Confirm Password">
-            </div>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-primary">Submit</button>
-          </div>
-        </div>
-      </div>
-    </div>
+		<form id="register-form" action="" method="post" role="form" style="display: none;">
+			<h2>REGISTER</h2>
+
+				<div class="form-group">
+					 <div class="row">
+					<div class="col">
+					 <input type="text" name="firstName" id="firstName" tabindex="1" class="form-control" placeholder="First Name" value="<?php if(isset($_POST['firstName'])) echo $_POST['firstName']; ?>">
+					</div>
+					<div class="col">
+					 <input type="text" name="lastName" id="lastName" tabindex="1" class="form-control" placeholder="Last Name" value="<?php if(isset($_POST['lastName'])) echo $_POST['lastName']; ?>">
+					</div>
+				</div>
+			</div>
+				<div class="form-group">
+					<input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password">
+				</div>
+				<div class="form-group">
+					<input type="password" name="confirm_password" id="confirm-password" tabindex="2" class="form-control" placeholder="Confirm Password">
+				</div>
+				<div class="form-group">
+					<div class="row">
+						<div class="col-sm-6 col-sm-offset-3">
+							<input type="submit" name="register-user" id="register-submit" tabindex="4" class="form-control btn btn-register" value="Register Now">
+						</div>
+					</div>
+				</div>
+		</form>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js"></script>
     <script src="https://npmcdn.com/tether@1.2.4/dist/js/tether.min.js"></script>
