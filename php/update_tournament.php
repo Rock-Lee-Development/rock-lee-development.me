ef55f5e37811c15792e52ec6769bed0cdf907e03
ian-fitzpatrick <ibf453@lindenwood.edu>
2018-04-11 12:43:29 -0500

    a few cha changes still testing

---
+++ php/update_tournament.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ian
+ * Date: 4/10/2018
+ * Time: 10:27 AM
+ */
+
+echo
+    "<div class=\"alert alert-success\" role=\"alert\">".
+        "<strong>Well done!</strong> You successfully read <a href=\"#\" class=\"alert-link\">this important alert message</a>".
+    "</div>";
+?>
