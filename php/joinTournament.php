a210bce7d74e50bc2b1f373523b737f09415d36f
keithdjouba09 <marlondjouba09@gmail.com>
2018-04-12 11:30:32 -0500

    changes

--- php/joinTournament.php
+++ php/joinTournament.php
@@ -8,25 +8,28 @@ if(!isset($message)) {
   $email = $_SESSION["email"];
   $query = "SELECT UserID FROM User WHERE email='$email'";
 $current_id = $db_handle->getUserID($query);
-$query = "INSERT INTO UserTournaments (TournamentID, UserID) VALUES
-('" . $_POST["TMname"] . "', '$current_id')";
-$insideTable = $db_handle->insertQuery($query);
-if(!empty($insideTable)) {
+
   //if it is a team game
-if(!empty($_POST["teamNmae"]))
+if(!empty($_POST["TeamName"]))
 {
+
+
     //check limit size
     $result = "SELECT COUNT(*) FROM TeamMembers WHERE TeamID = ".$_POST["teamName"];
     $row_count = $db_handle->numRows($result);
 
-    $result = "SELECT TeamLimit FROM UserTournaments WHERE TournamentID =".$_POST["TMname"]."AND TeamID = ".$_POST["teamNmae"];
+    $result = "SELECT TeamLimit FROM UserTournaments WHERE TournamentID =".$_POST["TMname"]."AND TeamID = ".$_POST["teamName"];
     $team_limit=$db_handle->getTeamLimit($result);
 
-    if($row_count<$team_limit)
+    if($team_limit>)//number of UserID rows in Teammbers table
     {
+
+    $query = "INSERT INTO UserTournaments (TournamentID, UserID) VALUES
+    ('" . $_POST["TMname"] . "', '$current_id')";
+    $insideTable = $db_handle->insertQuery($query);
         //enroll into team
         $query ="INSERT INTO TeamMembers (TeamID,UserID) VALUES
-('" . $_POST["teamNmae"] . "', '$current_id')";
+        ('" . $_POST["teamName"] . "', '$current_id')";
         $insideTable = $db_handle->insertQuery($query);
 
         if(!empty($insideTable)){
@@ -48,12 +51,18 @@ if(!empty($_POST["teamNmae"]))
     }
 
 
+}else{
+  $query = "SELECT UserID FROM User WHERE email='$email'";
+$current_id = $db_handle->getUserID($query);
+$query = "INSERT INTO UserTournaments (TournamentID, UserID) VALUES
+('" . $_POST["TMname"] . "', '$current_id')";
+$insideTable = $db_handle->insertQuery($query);
 }
 
   echo "<script> alert('you successfuly join this tournament');
            window.location.href='../index.html'; </script>";
 
-}
+
 
 }else{
   echo "<script> alert('something wrong');
