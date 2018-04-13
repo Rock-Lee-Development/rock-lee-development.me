6c151d6a5ef9e65fb99049755278d12ba2c1ddfc
keithdjouba09 <marlondjouba09@gmail.com>
2018-04-13 00:35:00 -0500

    ...

--- js/registration.js
+++ js/registration.js
@@ -1,4 +1,4 @@
-
+/*
 $('#register-submit').on('click', function validatemyForm() {
       var x = document.forms["myForm"]["newEmail"].value;
       var atpos = x.indexOf("@lindenwood");
@@ -7,4 +7,4 @@ $('#register-submit').on('click', function validatemyForm() {
           alert("This service is provided to students of Lindenwood Univerity, and as such you must provide a valid Lindenwood Email address in order to register");
           return false;
       }
-  });
+  });*/
