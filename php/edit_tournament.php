3ebba093ad740ff2f35b7444f0389253a595fbfc
Zoe Shang <ccjumpper@gmail.com>
2018-04-29 01:42:53 -0500

    Get input team name

--- php/edit_tournament.php
+++ php/edit_tournament.php
@@ -5,27 +5,38 @@ require_once("DBController.php");
 $db_handle = new DBController();
 
 //get tournament id
-//check number of team
-//find how many matches need
-//get winner id
-//get loser id
-$index = 1;
+$tm_id = $_POST["records"];
 
-for($i = 1;$i<15;$i++)
+//check number of team
+$query = "SELECT COUNT(*) FROM Team WHERE TournamentID = '$tm_id' ";
+$team_num = $db_handle->getCount($query);
+
+//find how many matches need
+$matches = $team_num -2;
+
+//get winner id - t_num
+//get loser id -
+
+//final winner
+$final = $_POST["final"];
+
+
+for($i = 1;$i<=$matches;$i++)
 {
+
     $win1 = $_POST["t".$i];
     if(!empty($win1))
     {
-        echo "<script> alert('starting update record $win1');
+        echo "<script> alert('starting update record $win1 with tournament id $tm_id');
            </script>";
     }else{
         echo "<script> alert('game not recorded');
            </script>";
-        break;
+
     }
 }
 
-$final = $_POST["final"];
+
 
 //$query = "INSERT INTO Matches(MatchType,TournamentID,WinnerID,LoserID)
 //VALUE ('team','','$win1')";
