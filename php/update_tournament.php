3855e21294d0809061068dc675d07dce26347e59
Zoe Shang <ccjumpper@gmail.com>
2018-05-03 23:11:36 -0500

    Comments

--- php/update_tournament.php
+++ php/update_tournament.php
@@ -1,37 +1,31 @@
 <?php
-//get tm id
-//get des
-//get name
-//update
+/*
+Author: Zhuocheng Shang
+Description: This file if set to update a tournament which is already approved
+            
+*/
+
 if(isset($_POST['get_id'])) {
 
-    // Create connection
+        // Create connection
     require_once("DBController.php");
     $db_handle = new DBController();
 
-
+        //get related tournament id, name , descrption
     $tmID = $_POST['get_id'];
     $name = $_POST['tm_name'];
     $des = $_POST['desc'];
-    //delete tounament
+             
+        //update tournament 
     $query = "UPDATE Tournament Set Name = \"$name\", Descripton = \"$des\" WHERE TournamentID = '$tmID' ";
-
-    //
-    //delete other table relate with this id
-    //UserTournaments
-    //team
-    //teammember
-    //records
-    //matches
-
     $result = $db_handle->updateQuery($query);
 
     if ($result) {
 
-        echo "<script> alert('you successfully update a tournament $tmID') </script>";
+        echo "<script> alert('You successfully update a tournament.') </script>";
 
     } else {
-        echo "<script> alert('wrong $tmID') </script>";
+        echo "<script> alert('Connect Fail.') </script>";
     }
 
 }
